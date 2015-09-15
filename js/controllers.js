ba92d35556d46eb5f5166b913eccef134d00124f
hemant <hemant3370@Gmail.com>
2015-09-15 20:48:34 +0530

    Update controllers.js

--- js/controllers.js
+++ js/controllers.js
@@ -1,7 +1,7 @@
 var mandiControllers = angular.module('mandiControllers', []);
 
 mandiControllers.controller('ListController' , ['$scope', '$http',function($scope, $http) {
-$http.get('https://data.gov.in/api/datastore/resource.json?resource_id=9ef84268-d588-465a-a308-a864a43d0070&api-key=84774f4bb9dc59c6e753bfea7e3e2e5e&limit=999').success(function(data) {
+$http.get('https://data.gov.in/api/datastore/resource.json?resource_id=9ef84268-d588-465a-a308-a864a43d0070&api-key=YOUR KEY').success(function(data) {
 $scope.mandi = data;
 $scope.mandiOrder = 'commodity';
 });
@@ -9,7 +9,7 @@ $scope.mandiOrder = 'commodity';
 }]);
 
 mandiControllers.controller('DetailsController' , ['$scope', '$http','$routeParams',function($scope, $http, $routeParams) {
-$http.get('https://data.gov.in/api/datastore/resource.json?resource_id=9ef84268-d588-465a-a308-a864a43d0070&api-key=84774f4bb9dc59c6e753bfea7e3e2e5e&limit=999').success(function(data) {
+$http.get('https://data.gov.in/api/datastore/resource.json?resource_id=9ef84268-d588-465a-a308-a864a43d0070&api-key=YOUR KEY').success(function(data) {
 $scope.mandi = data;
 $scope.whichItem = $routeParams.itemId;
 
