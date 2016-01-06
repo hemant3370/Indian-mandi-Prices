597289bf1b89b3d2e41468cb5933a70c1213eb8c
hemant <hemant3370@Gmail.com>
2016-01-06 15:43:52 +0530

    Update controllers.js

--- js/controllers.js
+++ js/controllers.js
@@ -2,16 +2,28 @@ var mandiControllers = angular.module('mandiControllers', []);
 var url = null;
 mandiControllers.controller('ListController' , ['$scope', '$http',function($scope, $http) {
  
-
+try{
 $scope.url = 'https://data.gov.in/api/datastore/resource.json?resource_id=9ef84268-d588-465a-a308-a864a43d0070&api-key=84774f4bb9dc59c6e753bfea7e3e2e5e';
 url = 'https://data.gov.in/api/datastore/resource.json?resource_id=9ef84268-d588-465a-a308-a864a43d0070&api-key=84774f4bb9dc59c6e753bfea7e3e2e5e';
 $http.get($scope.url).success(function(data) {
-$scope.mandi = data;})
+ if( supports_html5_storage() ){
+        localStorage.setItem('json_data', JSON.stringify(data))
+    }
+$scope.mandi = data;
+}
+catch(e){
+ var userData = JSON.parse(localStorage.getItem('json_data'));
+        if(userData !== null){
+            $scope.mandi = userData;
+        }
+}
+})
 $scope.ListController = function(){
 
 $scope.url = 'https://data.gov.in/api/datastore/resource.json?resource_id=9ef84268-d588-465a-a308-a864a43d0070&api-key=84774f4bb9dc59c6e753bfea7e3e2e5e&filters['+$scope.mandiOrder+']='+$scope.key;
 url =  'https://data.gov.in/api/datastore/resource.json?resource_id=9ef84268-d588-465a-a308-a864a43d0070&api-key=84774f4bb9dc59c6e753bfea7e3e2e5e&filters['+$scope.mandiOrder+']='+$scope.key;
 $http.get($scope.url).success(function(data) {
+ 
 $scope.mandi = data;})
 }
 }]);
@@ -19,9 +31,12 @@ $scope.mandi = data;})
 
 
 mandiControllers.controller('DetailsController' , ['$scope', '$http','$routeParams',function($scope, $http, $routeParams) {
- 
+ try{
 $http.get(url).success(function(data) {
-
+if( supports_html5_storage() ){
+        localStorage.setItem('json_data', JSON.stringify(data))
+    }
+    
 $scope.mandi = data;
 $scope.whichItem = $routeParams.itemId;
 
@@ -37,6 +52,15 @@ $scope.nextItem = Number($routeParams.itemId)+1;
 else{
 $scope.nextItem = 0;
 }
+}
+}
+catch(e){
+ var userData = JSON.parse(localStorage.getItem('json_data'));
+        if(userData !== null){
+            $scope.mandi = userData;
+        }
+}
+
 });
 
 }]);
